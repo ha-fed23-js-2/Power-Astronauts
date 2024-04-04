021ea306c151e7ed6112d3538c20fb1c0bd81708
wilmaniklasson <wilma.niklasson2@hotmail.se>
2024-04-04 16:19:50 +0200

    hämtat data från js filen till order page

--- src/routes/OrderPage.jsx
+++ src/routes/OrderPage.jsx
@@ -1,8 +1,23 @@
+import menuItems from "../data/menuItems";
+import "./LandingPage.css";
 const OrderPage = () => {
     return (
+        <>
         <div>
             <h1>Order Page</h1>
         </div>
+        <div>
+      {menuItems.map((item, index) => (
+        <div key={index}>
+          <h2>{item.name}</h2>
+          <p>{item.description}</p>
+          <p>Pris: {item.price} kr</p>
+          <img src={item.image} alt={item.name} />
+        </div>
+      ))}
+    </div>
+        </>
+        
     )
 }
 
