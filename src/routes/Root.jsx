d36eb9c88f70561a33c4295239b037e50c20a7d8
Grunkans <143597576+Grunkans@users.noreply.github.com>
2024-04-05 13:55:37 +0200

    header och startbild

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,19 +1,21 @@
 import { Link, NavLink, Outlet } from "react-router-dom"
 import './root.css'
+import Logga from "../assets/Logga.svg"
 
 // Router motsvarar App-komponenten
 // Består av en statisk del (visas alltid) och en dynamisk (Outlet ersätts av andra komponenter)
 const Root = () => (
 	<div className="app">
 		<header>
-			<h1> Kalssons Husmanskost </h1>
-			<nav>
-				<NavLink to="/"> LandingPage </NavLink>
+		
+			<nav className="buttons">
+				<span><NavLink to= "/" > LandingPage </NavLink>
 				<NavLink to="/OrderPage"> Orderpage </NavLink>
 				<NavLink to="/ConfirmPage"> ConfirmPage </NavLink>
                 <NavLink to="/LoginPage"> Log in </NavLink>
-                <NavLink to="/EditPage"> EditPage </NavLink>
+                <NavLink to="/EditPage"> EditPage </NavLink></span>
 			</nav>
+			<img className="loggaHeader" src={Logga} alt="Karlssons husmanskost logga" />
 		</header>
 
 		<main>
