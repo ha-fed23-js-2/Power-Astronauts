591fb369a72ceab9ace7b2f421bc3187c356cfcc
Grunkans <143597576+Grunkans@users.noreply.github.com>
2024-04-08 15:00:09 +0200

    framsida stylad

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -2,6 +2,7 @@ import { Link, NavLink, Outlet } from "react-router-dom"
 import './root.css'
 import Logga from "../assets/Logga.svg"
 
+
 // Router motsvarar App-komponenten
 // Består av en statisk del (visas alltid) och en dynamisk (Outlet ersätts av andra komponenter)
 const Root = () => (
@@ -12,7 +13,6 @@ const Root = () => (
 				<a><NavLink to= "/" > LandingPage </NavLink>
 				<NavLink to="/OrderPage"> Orderpage </NavLink>
 				<NavLink to="/ConfirmPage"> ConfirmPage </NavLink>
-                <NavLink to="/LoginPage"> Log in </NavLink>
                 <NavLink to="/EditPage"> EditPage </NavLink></a>
 			</nav>
 			<img className="loggaHeader" src={Logga} alt="Karlssons husmanskost logga" />
@@ -23,7 +23,26 @@ const Root = () => (
 		</main>
 
 		<footer>
-			innehåll
+			<div>
+			<h2>Adress</h2>
+			<p>Gustaf Adolfs Torg 3,
+				411 10 Göteborg
+				Sverige</p>
+			</div>
+			
+			<div>
+			<h2>Öppentider</h2>
+			<p>Måndag - Fredag: 17:00 - 21:00
+				Lördag - Söndag: 12:00 - 21:00 </p>
+				</div>
+
+				<div>
+			<h2>Kontakta oss</h2>
+			<p>Telefon: 031-13 83 83
+				Email:info@kalssons.se </p>
+				</div>
+
+				<NavLink to="/LoginPage"> Log in </NavLink>
 		</footer>
 	</div>
 )
