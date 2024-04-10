9cebac6f832d9023862ab05489c304747ea6dc75
wilmaniklasson <wilma.niklasson2@hotmail.se>
2024-04-10 21:40:35 +0200

    css till LandingPage

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'; 
+import '../routes/root.css'
+
+function Footer() {
+   
+
+    return (
+        <>
+        <div className="footer-container">
+                <div className="info">
+                    <h3>Adress</h3>
+                    <address>
+                        Gustaf Adolfs Torg 3,<br />
+                        411 10 Göteborg<br />
+                        Sverige
+                    </address>
+                </div>
+
+                <div className="info">
+                    <h3>Öppentider</h3>
+                    <p>Måndag - Fredag: 17:00 - 21:00</p>
+                    <p>Lördag - Söndag: 12:00 - 21:00</p>
+                </div>
+
+                <div className="info">
+                    <h3>Kontakta oss</h3>
+                    <p>Telefon: 031-13 83 83</p>
+                    <p>Email: <a href="mailto:info@kalssons.se">info@kalssons.se</a></p>
+                </div>
+
+                <div className="login-btn">
+                    <NavLink to="/LoginPage">Log in</NavLink>
+                </div>
+
+            </div>
+        </>
+        
+            
+        
+    );
+}
+
+export default Footer;
--- src/components/LandingPageCard.jsx
+++ src/components/LandingPageCard.jsx
@@ -1,9 +1,21 @@
-export default function LandingPageCard(props) {
-    return (
-    <div className="landing-card">
-        <h2 className="namn">{props.item.name}</h2>
-        <p className="beskrivning">{props.item.description}</p>
-        <p className="pris">Pris: {props.item.price} kr</p>
-    </div>
-    )
-}
+import { useStore } from '../data/menuItems';
+import React from 'react';
+import '../routes/LandingPage.css';
+
+const LandingPageCard = () => {
+  const menuItems = useStore((state) => state.menuItems);
+
+  return (
+    <>
+      {menuItems.map((item) => (
+        <div className="landing-card" key={item.id}>
+          <h2 className="landing-card-namn">{item.name}</h2>
+          <p className="beskrivning">{item.description}</p>
+          <p className="pris">{item.price} kr</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default LandingPageCard;
