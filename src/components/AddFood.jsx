4fb31dced711be03e768702a709ab8ebf340c02a
wanderingkitty <alinaericson95@gmail.com>
2024-04-10 13:23:06 +0200

    add menu item function

---
+++ src/components/AddFood.jsx
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+import { useStore } from '..data/store'
+
+function AddFood() {
+    const {addFood } = useStore((state) => { return { addFood: state.addFood} })
+
+    const [name, setName] = useState('')
+    const [image, setImage] = useState ('')
+    const [price, setPrice] = useState ('')
+    const [description, setDescription] = useState('')
+
+    function handleAdd() {
+        const newFood = {
+            name: name,
+            image: image,
+            price: price,
+            description: description
+        }
+        addFood(newFood)
+    }
+
+    return (
+        <section>
+            <h2>Lägg till ny rätt</h2>
+            <label>Namn:</label>
+            <input type="text" onChange={(e)=> setName(e.target.value)}></input>
+            <label>Pris:</label>
+            <input type="text" onChange={(e)=> setPrice(e.target.value)} />
+            <label>Beskrivning:</label>
+            <input type="text" onChange={(e)=> setDescription(e.target.value)} />
+            <button onClick={handleAdd}>Lägg till</button>
+        </section>
+    )
+}
+
+export default AddFood
