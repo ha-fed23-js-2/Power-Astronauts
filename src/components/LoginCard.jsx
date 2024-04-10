ec98086941d35bdae09066ff2d2c2c06064c8db2
Liam Bard <liam.axel.bard@gmail.com>
2024-04-10 15:47:26 +0200

    länk och kopplingar

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -1,44 +1,45 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginCard = () => {
 
-    const [password, setPassword] = useState('')
-    const [errorMessage, setErrorMessage] = useState('')
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     function checkPassword() {
         if (password.length === 0) {
-            setErrorMessage('Lösenordsfältet kan inte vara tomt.')
-        }
-        else if (password != 'mums') {
-            setErrorMessage('Fel lösenord.')
-        }
-        else {
-            setErrorMessage('')
+            setErrorMessage('Lösenordsfältet kan inte vara tomt.');
+        } else if (password !== 'mums') {
+            setErrorMessage('Fel lösenord.');
+        } else {
+            setErrorMessage('');
         }
     }
 
-
-    return (<div>
-        <div className='container'>
-            <div className="header">
-                <div className="text">Sign Up</div>
-                <div className="underline"></div>
-            </div>
-            <div className="input-frame">
-                <div className="input">
-                    <input type="text" placeholder="Name" required />
+    return (
+        <div>
+            <div className='container'>
+                <div className="header">
+                    <div className="text">Sign Up</div>
+                    <div className="underline"></div>
                 </div>
-                <div className="input">
-                    <input type="Password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
+                <div className="input-frame">
+                    <div className="input">
+                        <input type="text" placeholder="Name" required />
+                    </div>
+                    <div className="input">
+                        <input type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
+                    </div>
+                    <p>{errorMessage}</p>
                 </div>
-                <p>{errorMessage}</p>
-            </div>
-            <div className="submit-container">
-                <button className="submit" onClick={checkPassword}>Log In</button>
+                <div className="submit-container">
+                    <Link to="/EditPage"> {/* Använd Link för att navigera till EditPage */}
+                        <button className="submit" onClick={checkPassword}>Log In</button>
+                    </Link>
+                </div>
             </div>
         </div>
-    </div>
-    )
+    );
 }
 
-export default LoginCard
+export default LoginCard;
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -2,60 +2,48 @@ import { Link, NavLink, Outlet } from "react-router-dom"
 import './root.css'
 import Logga from "../assets/Logga.svg"
 
-
-// Router motsvarar App-komponenten
-// Består av en statisk del (visas alltid) och en dynamisk (Outlet ersätts av andra komponenter)
 const Root = () => (
-	<div className="app">
-		<header>
-		
-			<nav className="buttons">
-				<a><NavLink to= "/" > LandingPage </NavLink>
-				<NavLink to="/OrderPage"> Orderpage </NavLink>
-				<NavLink to="/ConfirmPage"> ConfirmPage </NavLink>
-                <NavLink to="/EditPage"> EditPage </NavLink></a>
-			</nav>
-			
-			<img className="loggaHeader" src={Logga} alt="Karlssons husmanskost logga" />
-			<div className="header-row">
-			<h1 className="header-options">Beställ</h1>
-			<h1 className="header-options">Meny</h1>
-			</div>
-			
-		</header>
-
-		<main>
-			<Outlet />
-		</main>
-
-		<footer>
-			<div className="info">
-			<h3>Adress</h3>
-			<p>Gustaf Adolfs Torg 3,
-				411 10 Göteborg
-				Sverige</p>
-			</div>
-			
-			<div className="info">
-				<h3>Öppentider</h3>
-				<p>Måndag - Fredag: 17:00 - 21:00</p>
-				<p>Lördag - Söndag: 12:00 - 21:00</p>
-			</div>
-
-				<div className="info">
-			<h3>Kontakta oss</h3>
-			<p>Telefon: 031-13 83 83
-				Email:info@kalssons.se </p>
-				</div>
-				<div className="login-btn">
-				<a><NavLink to="/LoginPage"> Log in </NavLink>
-					</a>
-
-				</div>
-
-				
-		</footer>
-	</div>
+    <div className="app">
+        <header>
+            <nav className="buttons">
+                <a>
+                    <NavLink to= "/" > LandingPage </NavLink>
+                    <NavLink to="/OrderPage"> Orderpage </NavLink>
+                    <NavLink to="/ConfirmPage"> ConfirmPage </NavLink>
+                    <NavLink to="/EditPage"> EditPage </NavLink>
+                </a>
+            </nav>
+            <img className="loggaHeader" src={Logga} alt="Karlssons husmanskost logga" />
+            <div className="header-row">
+                <NavLink to="/OrderPage" className="header-options">Beställ</NavLink>
+				<NavLink to="/" className="header-options">Meny</NavLink>
+            </div>
+        </header>
+        <main>
+            <Outlet />
+        </main>
+        <footer>
+            <div className="info">
+                <h3>Adress</h3>
+                <p>Gustaf Adolfs Torg 3,
+                    411 10 Göteborg
+                    Sverige</p>
+            </div>
+            <div className="info">
+                <h3>Öppentider</h3>
+                <p>Måndag - Fredag: 17:00 - 21:00</p>
+                <p>Lördag - Söndag: 12:00 - 21:00</p>
+            </div>
+            <div className="info">
+                <h3>Kontakta oss</h3>
+                <p>Telefon: 031-13 83 83
+                    Email:info@kalssons.se </p>
+            </div>
+            <div className="login-btn">
+                <a><NavLink to="/LoginPage"> Log in </NavLink></a>
+            </div>
+        </footer>
+    </div>
 )
 
 export default Root
