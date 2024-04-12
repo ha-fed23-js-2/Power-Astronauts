38b6801418bb884af67e24df8e8e6da6b0941b53
Anna <acarlssong@hotmail.com>
2024-04-12 11:39:55 +0200

    Loginfunktionalitet, error message styling

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -15,14 +15,14 @@
 //             setErrorMessage('');
 //         }
 //     }
-    
+
 //     const [name, setName] = useState('');
 //     const [nameTouched, setNameTouched] = useState(false);
 
 //     const nameIsValid = name.length > 0;
 //     const nameErrorMessage = nameIsValid ? '' : 'Vänligen, fyll i ditt namn'; 
 //     const isSubmitDisabled = password.length === 0 || password !== 'mums';
-    
+
 //     let nameErrorClass = 'error ', nameClass = '';
 //     if (!nameTouched) {
 //         nameErrorClass += 'hidden';
@@ -45,7 +45,7 @@
 //                     <div className="input">
 //                         <input type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
 //                     </div>
-                    
+
 //                 </div>
 //                 <div className="submit-container">
 //                      <Link to="/EditPage"> {/*Använd Link för att navigera till EditPage */}
@@ -63,41 +63,60 @@ import { Link } from 'react-router-dom';
 
 const LoginCard = () => {
 
+    const [name, setName] = useState('');
+    const [nameTouched, setNameTouched] = useState(false);
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+
+    function checkUsername() {
+        if (name.length === 0) {
+            setNameTouched(true);
+        }
+    }
 
     function checkPassword() {
         if (password.length === 0) {
             setErrorMessage('Lösenordsfältet kan inte vara tomt.');
         } else if (password !== 'mums') {
             setErrorMessage('Fel lösenord.');
-        } else {
+        }
+        else {
             setErrorMessage('');
         }
     }
+
+    function validateForm() {
+        checkUsername()
+        checkPassword()
+    }
+
     // const passwordIsValid = password === ('mums');
     // const passwordErrorMessage = passwordIsValid ? '' : 'Lösenordet är felaktigt';
 
-    const [name, setName] = useState('');
-    const [nameTouched, setNameTouched] = useState(false);
 
     const nameIsValid = name.length > 0;
-    const nameErrorMessage = nameIsValid ? '' : 'Vänligen, fyll i ditt namn';
+    const nameErrorMessage = nameIsValid ? '' : 'Vänligen fyll i ditt namn.';
     const isSubmitDisabled = password.length === 0 || password !== 'mums' || !nameIsValid;
-    
+
     let nameErrorClass = 'error ', nameClass = '';
     if (!nameTouched) {
         nameErrorClass += 'hidden';
     } else {
         nameErrorClass += nameIsValid ? 'hidden' : 'invalid';
         nameClass += nameIsValid ? 'valid' : 'invalid';
-    }  
+    }
     // let passwordErrorClass = 'error ', passwordClass = '';
     // if (passwordIsValid) {
     //     passwordErrorClass += 'hidden';
     // } else {
     //     passwordErrorClass += passwordIsValid ? 'hidden' : 'invalid';
     //     passwordClass += passwordIsValid ? 'valid' : 'invalid'; 
+    // }
+
+    // onKeyUp={(e) => {
+    //    if (e.key === "Enter"){
+    //     validateForm;
+    // }
     // }
 
     return (
@@ -108,27 +127,31 @@ const LoginCard = () => {
                     <div className="underline"></div>
                 </div>
                 <div className="input-frame">
-                <div class="input">
-                            <input type="text" placeholder="Namn" required 
+                    <div className="input">
+                        <input type="text" placeholder="Namn" required
                             className={nameClass}
                             value={name}
                             onChange={event => setName(event.target.value)}
                             onBlur={() => setNameTouched(true)}
-                            />
-                    <p className={nameErrorClass}>{nameErrorMessage} &nbsp;</p>
+                        />
+                        <p className={nameErrorClass}>{nameErrorMessage} &nbsp;</p>
                     </div>
                     <div className="input">
-                    <div className="input">
-                        <input type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
-                    </div>
+                        <div className="input">
+                            <input type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
+                            <p className="password-error">{errorMessage}</p>
                         </div>
-                    <p className="error">{errorMessage}</p>
+                    </div>
                 </div>
-                <div className="submit-container">
-                    <Link to="/EditPage">
-                        <button className="submit" onClick={checkPassword} disabled={isSubmitDisabled}>Logga in</button>
-                    </Link>
+                {isSubmitDisabled ? <div className="submit-container">
+                    <button className="submit" onClick={validateForm}>Logga in</button>
                 </div>
+                    :
+                    <div className="submit-container">
+                        <Link to="/EditPage">
+                            <button className="submit">Logga in</button>
+                        </Link>
+                    </div>}
             </div>
         </div>
     );
